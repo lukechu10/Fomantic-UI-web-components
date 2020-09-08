3d36f131c1ff59f00a186b03ae5d58315c70d48c
Luke Chu <lukewchu@outlook.com>
2020-09-08 23:35:35 +0000

    chore: configure storybook

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+	input: 'src/main.ts',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'cjs'
+	},
+	plugins: [
+		typescript(/*{ plugin options }*/)
+	]
+}
